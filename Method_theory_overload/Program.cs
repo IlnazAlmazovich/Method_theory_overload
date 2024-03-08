cf3d2c4920c734cb325101d3bbf49a879e214b92
farhullin2017@gmail.com <90451035+IlnazFarkhullin@users.noreply.github.com>
2024-03-08 18:59:45 +0300

    Перегрузка методов

---
+++ Method_theory_overload/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Method_theory_overload
+{
+    class Calculator //создаём класс
+    {
+        public int result; //поле для хранения результатов
+
+        public void Addition(int a, int b) //метод для вычисления суммы двух чисел
+        {
+            result = a + b;
+            Console.WriteLine($"Результат сложения двух чисел {result}");
+        }
+        public void Addition(int a, int b,int c) //метод для вычисления суммы трёх чисел
+        {
+            result = a + b + c;
+            Console.WriteLine($"Результат сложения двух чисел {result}");
+        }
+
+        public void Addition(int a, int b, int c, int d)//метод для вычисления суммы четырёх чисел
+        {
+            result = a + b + c + d;
+            Console.WriteLine($"Результат сложения двух чисел {result}");
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Calculator calculator = new Calculator();//создание экземпляра класса
+            Console.WriteLine("Введите a: ");
+            int a = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Введите b: ");
+            int b = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Введите c: ");
+            int c = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Введите d: ");
+            int d = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Результат первого метода: ");
+            calculator.Addition(a, b); //вызов метода с 2мя параметрами
+            Console.WriteLine();
+            Console.WriteLine("Результат второго метода: ");
+            calculator.Addition(a, b, c);// вызов метода с 3мя параметрами
+            Console.WriteLine();
+            Console.WriteLine("Результат третьего метода: ");
+            calculator.Addition(a, b, c, d);// вызов метода с 4мя параметрами
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Method_theory_overload/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанные с этой сборкой.
+[assembly: AssemblyTitle("Method_theory_overload")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Method_theory_overload")]
+[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// из модели COM задайте для атрибута ComVisible этого типа значение true.
+[assembly: ComVisible(false)]
+
+// Следующий GUID представляет идентификатор typelib, если этот проект доступен из модели COM
+[assembly: Guid("036d00c6-bb65-401b-ace2-033f11cc0617")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//      Номер сборки
+//      Номер редакции
+//
+// Можно задать все значения или принять номера сборки и редакции по умолчанию 
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
